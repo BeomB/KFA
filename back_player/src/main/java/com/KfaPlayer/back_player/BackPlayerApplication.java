efe9b59335e4826a3bd973a3ee846ed583076610
beombeom1119 <qjarl1119@naver.com>
2022-03-24 15:43:00 +0900

    back_end 연결

---
+++ back_player/src/main/java/com/KfaPlayer/back_player/BackPlayerApplication.java
@@ -0,0 +1,13 @@
+package com.KfaPlayer.back_player;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackPlayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackPlayerApplication.class, args);
+	}
+
+}
---
+++ back_player/src/test/java/com/KfaPlayer/back_player/BackPlayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KfaPlayer.back_player;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackPlayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
