94e3d84d2f32a33da388ddb220f35a79e737d9d3
beombeom1119 <qjarl1119@naver.com>
2022-04-03 00:47:00 +0900

    docker CORS

--- back_player/src/main/java/com/KfaPlayer/back_player/BackPlayerApplication.java
+++ back_player/src/main/java/com/KfaPlayer/back_player/BackPlayerApplication.java
@@ -2,12 +2,25 @@ package com.KfaPlayer.back_player;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BackPlayerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackPlayerApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
 	}
 
 }
--- back_player/src/main/java/com/KfaPlayer/back_player/controller/GameController.java
+++ back_player/src/main/java/com/KfaPlayer/back_player/controller/GameController.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin("*")
+
+//@CrossOrigin("*")
 
 @RestController
 public class GameController {
