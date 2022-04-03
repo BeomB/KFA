bcad0b2159777aa241b60db84fb0132a5e410928
Beom <qjarl1119@naver.com>
2022-04-03 21:22:30 +0900

    docker method 오류 해결

--- back_player/src/main/java/com/KfaPlayer/back_player/BackPlayerApplication.java
+++ back_player/src/main/java/com/KfaPlayer/back_player/BackPlayerApplication.java
@@ -18,7 +18,7 @@ public class BackPlayerApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("*");
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST","PUT","DELETE");
 			}
 		};
 	}
